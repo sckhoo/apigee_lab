238ed23b3cf4a9cc0f84e874c81cc958a920ced3
sckhoo <sckhoo@gmail.com>
2022-10-02 14:19:30 +0800

    Added Dockerfile

--- API_google_sheet/todo.py
+++ API_google_sheet/todo.py
@@ -29,36 +29,24 @@ async def get_all_todos():
 
 @app.get('/todo/{id}')
 async def get_todo(id: int):
-
     try:
-        
         return store_todo[id]
-    
     except:
-        
         raise HTTPException(status_code=404, detail="Todo Not Found")
 
 @app.put('/todo/{id}')
 async def update_todo(id: int, todo: Todo):
-
     try:
-
         store_todo[id] = todo
         return store_todo[id]
-    
     except:
-        
         raise HTTPException(status_code=404, detail="Todo Not Found")
 
 @app.delete('/todo/{id}')
 async def delete_todo(id: int):
-
     try:
-
         obj = store_todo[id]
         store_todo.pop(id)
         return obj
-    
     except:
-        
         raise HTTPException(status_code=404, detail="Todo Not Found")
